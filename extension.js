50ff409f8080f415ccc2303f87aa8217676c256d
Nik Henri <nhenri@orthogone.com>
2021-05-12 10:37:26 -0400

    able to find the type

--- extension.js
+++ extension.js
@@ -7,7 +7,7 @@ let a = `module rand_num_generator#(
 )
 
 (
-    input wire clk, reset, 
+    input wire clk, reset,
     output wire [N:0] q
 );
 
@@ -19,9 +19,9 @@ typedef struct packed {
     bit        valid;
     bit [31:0] data;
   } mem_pkt;
-   
+
   mem_pkt pkt;
-  
+
 pkt.
 endmodule
 `
@@ -44,27 +44,66 @@ function activate(context) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "test-nik" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('test-nik.testNik', function () {
-		let a = vscode.window.activeTextEditor
-		let b = vscode.window.activeTextEditor.document
-		let c = vscode.window.activeTextEditor.selection
-		console.log(`Path ${b.fileName}`)
-		console.log(`lauggage ${b.languageId}`)
-		console.log(`Text ${b.getText()}`)
-		console.log(`Text ${c.active.line}`)
-		console.log(`Text ${c.active.character}`)
-		//console.log(`Text ${b.getWordRangeAtPosition()}`)
-		//console.log(`Text ${b.lineAt()}`)
-		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from test Nik!');
-	});
+	let cnt = 0
 
-	// const provider1 = vscode.languages.registerCompletionItemProvider('systemverilog', {
+
+
+	const provider2 = vscode.languages.registerCompletionItemProvider(
+		'systemverilog',
+		{
+			provideCompletionItems(document, position) {
+				const linePrefix = document.lineAt(position).text.substr(0, position.character-1);
+
+				let match = linePrefix.match(/[A-Za-z]+\w*$/g) // start with a letter, followed by any nb of caracter
+				if (match) {
+					console.log("searching for signal name...")
+
+					const variableName = match[0]
+					console.log(`searching for variable '${variableName}'`)
+					text = document.getText()
+					// first word that is not input | output | inout
+					const declaration_type = text.match(`\\b(?!input\\s|output\\s|inout\\s)(\\w+).*${variableName}`)[1]
+					console.log(`Type is '${declaration_type}'`)
+
+					cnt += 1
+					return [
+						new vscode.CompletionItem(`Nik ${cnt}`, vscode.CompletionItemKind.Method),
+						//new vscode.CompletionItem('warn', vscode.CompletionItemKind.Method),
+					];
+				}
+			}
+		},
+		'.' // triggered whenever a '.' is being typed
+	);
+
+
+	context.subscriptions.push(disposable, provider2);
+}
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
+
+				// const line = document.lineAt(position).text
+
+				// const text = document.getText()
+				// console.log("----------")
+				// console.log(text)
+				// console.log("----------")
+
+				// let u = document.lineAt(position)
+				// console.log(document.lineAt(position))
+				// console.log("----------")
+				// console.log(position)
+				// console.log("----------")
+				// console.log(document.lineAt(position).text)
+
+					// const provider1 = vscode.languages.registerCompletionItemProvider('systemverilog', {
 
 	// 	provideCompletionItems(document, position, token, context) {
 
@@ -86,8 +125,8 @@ function activate(context) {
 	// 		commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
 
 	// 		// a completion item that retriggers IntelliSense when being accepted,
-	// 		// the `command`-property is set which the editor will execute after 
-	// 		// completion has been inserted. Also, the `insertText` is set so that 
+	// 		// the `command`-property is set which the editor will execute after
+	// 		// completion has been inserted. Also, the `insertText` is set so that
 	// 		// a space is inserted after `new`
 	// 		const commandCompletion = new vscode.CompletionItem('new');
 	// 		commandCompletion.kind = vscode.CompletionItemKind.Keyword;
@@ -103,61 +142,25 @@ function activate(context) {
 	// 		];
 	// 	}
 	// });
-	let cnt = 0
 
 
 
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	// let disposable = vscode.commands.registerCommand('test-nik.testNik', function () {
+	// 	let a = vscode.window.activeTextEditor
+	// 	let b = vscode.window.activeTextEditor.document
+	// 	let c = vscode.window.activeTextEditor.selection
+	// 	console.log(`Path ${b.fileName}`)
+	// 	console.log(`lauggage ${b.languageId}`)
+	// 	console.log(`Text ${b.getText()}`)
+	// 	console.log(`Text ${c.active.line}`)
+	// 	console.log(`Text ${c.active.character}`)
+	// 	//console.log(`Text ${b.getWordRangeAtPosition()}`)
+	// 	//console.log(`Text ${b.lineAt()}`)
+	// 	// The code you place here will be executed every time your command is executed
 
-
-
-
-	const provider2 = vscode.languages.registerCompletionItemProvider(
-		'systemverilog',
-		{
-			provideCompletionItems(document, position) {
-
-				// get all text until the `position` and check if it reads `console.`
-				// and if so then complete if `log`, `warn`, and `error`
-				//const linePrefix = document.lineAt(position).text.substr(0, position.character);
-				const line = document.lineAt(position).text
-			
-				if (!line.endsWith('.')) {
-					return undefined;
-				}
-
-				// const text = document.getText()
-				// console.log("----------")
-				// console.log(text)
-				// console.log("----------")
-
-				// let u = document.lineAt(position)
-				// console.log(document.lineAt(position))
-				// console.log("----------")
-				// console.log(position)
-				// console.log("----------")
-				// console.log(document.lineAt(position).text)
-				let variableName = line.match(/[A-Za-z]+\w*\.$/g)[0] //get the match
-				variableName = variableName.slice(0, -1) // remove the '.'
-				console.log(`searching for variable ${variableName}`)
-				cnt += 1
-				return [
-					new vscode.CompletionItem(`suck it Gui ${cnt}`, vscode.CompletionItemKind.Method),
-					//new vscode.CompletionItem('warn', vscode.CompletionItemKind.Method),
-					//new vscode.CompletionItem('error', vscode.CompletionItemKind.Method),
-				];
-			}
-		},
-		'.' // triggered whenever a '.' is being typed
-	);
-
-
-	context.subscriptions.push(disposable, provider2);
-}
-
-// this method is called when your extension is deactivated
-function deactivate() {}
-
-module.exports = {
-	activate,
-	deactivate
-}
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello World from test Nik!');
+	// });
