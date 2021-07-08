522ba437d01e4b8cf52b2bbba3fb130cdcebc362
Nik Henri <nhenri@orthogone.com>
2021-07-08 10:24:41 -0400

    refactor

--- src/diagnostic.js
+++ src/diagnostic.js
@@ -1,65 +1,74 @@
 const vscode = require('vscode')
-const utils = require('./utils')
-
-const path = require('path')
 const fs = require('fs')
 const os = require("os")
 const child_process = require('child_process');
+
+const utils = require('./utils')
+
 //----------------------------------------------------------------------------
-const collection = vscode.languages.createDiagnosticCollection('test');
+const collection = vscode.languages.createDiagnosticCollection('nik');
+const tempDir = os.tmpdir().replace(/\\/g,"/")
+console.log(`tempWorkDir: ${tempDir}`)
+const tempWorkDir = tempDir + "/work"
+const tempFilePath = tempDir + "/tmp.sv"
 
+if(fs.existsSync(tempWorkDir)) fs.rmdirSync(tempWorkDir, { recursive: true })
+
+//----------------------------------------------------------------------------
 async function updateDiagnostic() {
-	const tempDir = os.tmpdir().replace(/\\/g,"/")
-	console.log(tempDir)
+    console.log("Diagnostic")
+	await utils.getFileText() // init
+	let fileNameWithoutExt = utils.uriToFileNameWithoutExt(vscode.window.activeTextEditor.document.uri)
 
+    await saveCurrentTextToTemporayFile(fileNameWithoutExt, tempFilePath)
+    let fileStr = await getCompilationFileList(fileNameWithoutExt, tempFilePath)
+	let cmdStr = `vlog -incr -quiet -warning error -svinputport=relaxed -lint=default -suppress 2181,7061,2254 -work ${tempWorkDir} ${fileStr}`
+	console.log(cmdStr)
 
-	// const collection = vscode.languages.createDiagnosticCollection('test');
-	await utils.getFileText()
+	child_process.exec(cmdStr, (error, stdout, stderr) => {
+		// console.error(`exec error: ${error}`)
+		console.log(`stdout: ${stdout}`)
+		// console.error(`stderr: ${stderr}`)
+		if(error) {
+			let firstErrorInfo = getFirstErrorInfo(stdout)
 
-	let fileNameWithoutExt = "qmngr_tx_arbiter_events"
+			collection.set(vscode.window.activeTextEditor.document.uri, [
+                new vscode.Diagnostic(
+                    new vscode.Range(new vscode.Position(firstErrorInfo.line, 0), new vscode.Position(firstErrorInfo.line, 999)),
+                    firstErrorInfo.msg,
+                    vscode.DiagnosticSeverity.Error
+			    )])
+		} else {
+			collection.clear();
+		}
+	})
+}
 
-	let tempFilePath = tempDir + `/${fileNameWithoutExt}.sv`
-	let txt = (await utils.getFileText(fileNameWithoutExt)).text
-	fs.writeFileSync(tempFilePath, txt)
+//----------------------------------------------------------------------------
+async function saveCurrentTextToTemporayFile(fileNameWithoutExt, tempFilePath) {
+    let text = (await utils.getFileText(fileNameWithoutExt)).text
+	fs.writeFileSync(tempFilePath, text)
+}
 
-	// let tempFilePath = tempDir + `/${fileNameWithoutExt}.sv`
-	let importNameList = await utils.getImportNameListRecursive(fileNameWithoutExt)
-	//importNameList.unshift(fileNameWithoutExt)
+//----------------------------------------------------------------------------
+async function getCompilationFileList(fileNameWithoutExt, tempFilePath) {
+    let importNameList = await utils.getImportNameListRecursive(fileNameWithoutExt)
 	importNameList.reverse()
 	let fileStr = ""
 	for (let fileNameWithoutExtList of importNameList)
 		fileStr += await utils.getFilePath(fileNameWithoutExtList) + " "
 	fileStr += tempFilePath
-	let workPath = tempDir + "/work"
-	let cmdStr = `vlog -quiet -warning error -svinputport=relaxed -lint=default -suppress 2181,7061,2254 -work ${workPath} ${fileStr}`
-	console.log(cmdStr)
-
-	//child_process.exec("vdel -all")
-	fs.rmdirSync(workPath, { recursive: true });
-	child_process.exec(cmdStr, (error, stdout, stderr) => {
-		console.error(`exec error: ${error}`)
-		console.log(`stdout: ${stdout}`)
-		console.error(`stderr: ${stderr}`)
-		if (error) {
-			let matchAll = Array.from(stdout.matchAll(/\*\* Error: \(.*?\) ([^(]*)\((\d+)\): (.*)/g))
-			let filePath = matchAll[0][1]
-			let line = parseInt(matchAll[0][2])-1
-			let msg = matchAll[0][3]
-
-			collection.set(vscode.window.activeTextEditor.document.uri, [new vscode.Diagnostic(
-				new vscode.Range(new vscode.Position(line, 0), new vscode.Position(line, 999)),
-				msg,
-				vscode.DiagnosticSeverity.Errore
-			)])
-		//console.error(`exec error: ${error}`)
-		} else {
-			collection.clear();
-			console.log("No error");
-		}
-	})
+    return fileStr
 }
 
-
+//----------------------------------------------------------------------------
+function getFirstErrorInfo(text) {
+    let matchAll = Array.from(text.matchAll(/\*\* Error: \(.*?\) ([^(]*)\((\d+)\): (.*)/g))
+    let path = matchAll[0][1]
+    let line = parseInt(matchAll[0][2])-1
+    let msg = matchAll[0][3]
+    return {line, msg, path}
+}
 
 //----------------------------------------------------------------------------
 module.exports = {
