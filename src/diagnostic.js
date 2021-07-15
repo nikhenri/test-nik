2292fa6fc3e2bcac9d5974222ede0ea709745e2a
Nik Henri <nhenri@orthogone.com>
2021-07-15 18:55:54 -0400

    Refactor diagnostic

--- src/diagnostic.js
+++ src/diagnostic.js
@@ -12,19 +12,7 @@ const ouputChannel = require('./ouputChannel')
 //----------------------------------------------------------------------------
 const collection = vscode.languages.createDiagnosticCollection('nik')
 const tempDir = os.tmpdir().replace(/\\/g,"/")
-const tempWorkDir = tempDir + "/work"
-let incdirStr
 
-//----------------------------------------------------------------------------
-function loadWorkspaceConfig() {
-	let workspaceFolder = vscode.workspace.workspaceFolders[0].uri.fsPath.replace(/\\/g,"/")
-	let incdirList = vscode.workspace.getConfiguration('nik').get("incdir")
-	if(incdirList)
-		incdirStr = `+incdir+${workspaceFolder}/` + incdirList.join(` +incdir+${workspaceFolder}/`)
-	else
-		incdirStr = ""
-	console.log(`incdirStr: ${incdirStr}`)
-}
 //----------------------------------------------------------------------------
 // Save activeTextEditor text in file
 // search activeTextEditor dependancy
@@ -33,86 +21,140 @@ function loadWorkspaceConfig() {
 // Add error wave
 async function updateDiagnostic() {
     if(path.parse(vscode.window.activeTextEditor.document.uri.fsPath).ext == ".svh") return
-	ouputChannel.log("Diagnostic")
-	if(incdirStr == undefined) loadWorkspaceConfig()
-	await utils.getFileText() // init
-	if(fs.existsSync(tempWorkDir)) fs.rmdirSync(tempWorkDir, { recursive: true })
 
-	let fileNameWithoutExt = utils.uriToFileNameWithoutExt(vscode.window.activeTextEditor.document.uri)
-    let tempFilePath = tempDir + `/${fileNameWithoutExt}.sv`
-    await saveCurrentTextToTemporayFile(fileNameWithoutExt, tempFilePath)
-    let fileStr = await getCompilationFileList(fileNameWithoutExt, tempFilePath)
-	let filePath = vscode.window.activeTextEditor.document.uri.fsPath.replace(/\\/g,"/")
-	let fileDir = path.dirname(filePath)
-	let cmdStr = `vlog -quiet -warning error -svinputport=relaxed -lint=default -suppress 2181,7061,2254 +incdir+${fileDir} ${incdirStr} -work ${tempWorkDir} ${fileStr}`
-	ouputChannel.log(cmdStr)
+    ouputChannel.log("Diagnostic")
+    await utils.getFileText() // init
+    let fileNameWithoutExt = utils.uriToFileNameWithoutExt(vscode.window.activeTextEditor.document.uri)
 
-	child_process.exec(cmdStr, async (error, stdout, stderr) => {
-		if(error) {
-		    ouputChannel.log(stdout)
-			let msg, line
-			try {
-				let firstErrorInfo = getFirstErrorInfo(stdout, filePath)
-				msg = firstErrorInfo.msg
-				if(firstErrorInfo.fileNameWithoutExt != fileNameWithoutExt) {
-					let txt = (await utils.getFileText(fileNameWithoutExt)).text
-					let index = txt.match(new RegExp(`\\b${firstErrorInfo.fileNameWithoutExt}::`)).index
-					line = utils.indexToPosition(txt, index).line
-				} else {
-					line = firstErrorInfo.line
-				}
-			} catch (error) {
-				ouputChannel.log(`CRASH: ${error}`)
-				msg = stdout
-				line = 0
-			}
-
-
-			collection.set(vscode.window.activeTextEditor.document.uri, [new vscode.Diagnostic(
-                new vscode.Range(new vscode.Position(line, 0), new vscode.Position(line, 999)),
-                msg,
-                vscode.DiagnosticSeverity.Error
-            )])
-		} else {
-			ouputChannel.log("no error!\n")
-			collection.delete(vscode.Uri.file(filePath))
-		}
-	})
+    fs.mkdtemp(tempDir + '/work_', async (err, directory) => {
+        await saveCurrentTextToTemporayFile(fileNameWithoutExt, directory)
+        let cmdStr = await getCompilationCommand(fileNameWithoutExt, directory)
+        child_process.exec(cmdStr, (error, stdout) => {compilationCommandCallback(fileNameWithoutExt, directory, error, stdout)})
+    })
 }
 
 //----------------------------------------------------------------------------
-async function saveCurrentTextToTemporayFile(fileNameWithoutExt, tempFilePath) {
+// save the current live text in order to compile it
+async function saveCurrentTextToTemporayFile(fileNameWithoutExt, directory) {
+    let tempFilePath = getTempFilePath(fileNameWithoutExt, directory)
     let text = (await utils.getFileText(fileNameWithoutExt)).text
-	fs.writeFileSync(tempFilePath, text)
+    fs.writeFileSync(tempFilePath, text)
 }
 
 //----------------------------------------------------------------------------
-async function getCompilationFileList(fileNameWithoutExt, tempFilePath) {
+function getTempFilePath (fileNameWithoutExt, directory) {
+    return directory + `/${fileNameWithoutExt}.sv`
+}
+
+//----------------------------------------------------------------------------
+// get the vlog command to run in order to compile the file with pkg
+async function getCompilationCommand (fileNameWithoutExt, directory) {
+    let fileDir = path.dirname(vscode.window.activeTextEditor.document.uri.fsPath).replace(/\\/g,"/")
+    let incdirStr = getIncdirStrFromSettings()
+    let fileStr = await getCompilationFileList(fileNameWithoutExt, directory)
+    let cmdStr = `vlog -quiet -warning error -svinputport=relaxed -lint=default -suppress 2181,7061,2254 -work ${directory} +incdir+${fileDir} ${incdirStr} ${fileStr}`
+    ouputChannel.log(cmdStr)
+    return cmdStr
+}
+//----------------------------------------------------------------------------
+// get the string in order of the file that need to be compiled (pkg first)
+async function getCompilationFileList(fileNameWithoutExt, directory) {
+    let tempFilePath = getTempFilePath(fileNameWithoutExt, directory)
     let importNameList = await utils.getImportNameListRecursive(fileNameWithoutExt)
-	importNameList.reverse()
-	let fileStr = ""
-	for (let fileNameWithoutExtList of importNameList)
-		fileStr += await utils.getFilePath(fileNameWithoutExtList) + " "
-	fileStr += tempFilePath
+    importNameList.reverse()
+    let fileStr = ""
+    for (let fileNameWithoutExtList of importNameList)
+        fileStr += await utils.getFilePath(fileNameWithoutExtList) + " "
+    fileStr += tempFilePath
     return fileStr
 }
 
+//----------------------------------------------------------------------------
+// load the path put by the user, load once
+function getIncdirStrFromSettings() {
+    if(!getIncdirStrFromSettings.incdirStr) {
+        let workspaceFolder = vscode.workspace.workspaceFolders[0].uri.fsPath.replace(/\\/g,"/")
+        let incdirList = vscode.workspace.getConfiguration('nik').get("incdir")
+        if(incdirList)
+            getIncdirStrFromSettings.incdirStr = `+incdir+${workspaceFolder}/` + incdirList.join(` +incdir+${workspaceFolder}/`)
+        else
+            getIncdirStrFromSettings.incdirStr = ""
+    }
+    // console.log(`incdirStr: ${incdirStr}`)
+    return getIncdirStrFromSettings.incdirStr
+}
+
+//----------------------------------------------------------------------------
+// The function call after the compilation is done, analyse stdout and add/remove error
+async function compilationCommandCallback(fileNameWithoutExt, directory, error, stdout) {
+    fs.rmdir(directory, { recursive: true }, ()=>{})
+    if(error) {
+        ouputChannel.log(stdout)
+        let {line, msg} = await getLineAndMsgFromStdout(stdout, fileNameWithoutExt)
+        addErrorOnActiveTextEditor(line, msg)
+    } else {
+        ouputChannel.log("no error!\n")
+        removeErrorOnActiveTextEditor()
+    }
+}
+
+//----------------------------------------------------------------------------
+// from the stdout get the where (line) and what (msg) to display as error
+async function getLineAndMsgFromStdout(stdout, fileNameWithoutExt) {
+    let line, msg
+    try {
+        let firstErrorInfo = getFirstErrorInfo(stdout)
+        msg = firstErrorInfo.msg
+        if(firstErrorInfo.fileNameWithoutExt != fileNameWithoutExt) { // if from import file
+            line = await getImportLine(fileNameWithoutExt, firstErrorInfo.fileNameWithoutExt)
+        } else { // error is in current file
+            line = firstErrorInfo.line
+        }
+
+    } catch (error) {
+        ouputChannel.log(`CRASH: ${error}`)
+        msg = error
+        line = 0
+    }
+    return {line, msg}
+}
 //----------------------------------------------------------------------------
 // ** Error: C:/Users/nhenri/AppData/Local/Temp/tcp_regs.sv(478): (vlog-2730) Undefined variable: 'vg_Read_Datas'.
 // ** Error: (vlog-13069) C:/Users/nhenri/AppData/Local/Temp/tcp_regs.sv(32): near "input": syntax error, unexpected input, expecting ')'.
 // ** Error (suppressible): C:/Users/nhenri/AppData/Local/Temp/tcp_regs.sv(226): (vlog-2623) Undefined variable: i_cfg_axi_aclk_p.
 function getFirstErrorInfo(text) {
     let matchAll = Array.from(text.matchAll(/\*\* Error.*?:.*? ([^(]*)\((\d+)\): (?:\(.*?\) )?(.*)/g))
-	let fileNameWithoutExt = utils.filePathToFileNameWithoutExt(matchAll[0][1])
+    let fileNameWithoutExt = utils.filePathToFileNameWithoutExt(matchAll[0][1])
     let line = parseInt(matchAll[0][2])-1
     let msg = matchAll[0][3]
     return {line, msg, fileNameWithoutExt}
 }
 
 //----------------------------------------------------------------------------
-module.exports = {
-	updateDiagnostic,
-	loadWorkspaceConfig,
+// found the import:: line in a file
+async function getImportLine(fileNameWithoutExt, importName) {
+    let txt = (await utils.getFileText(fileNameWithoutExt)).text
+    let index = txt.match(new RegExp(`\\b${importName}::`)).index //seatch import
+    let line = utils.indexToPosition(txt, index).line
+    return line
+}
+//----------------------------------------------------------------------------
+// add error wave to text
+function addErrorOnActiveTextEditor (line, msg) {
+    collection.set(vscode.window.activeTextEditor.document.uri, [new vscode.Diagnostic(
+        new vscode.Range(new vscode.Position(line, 0), new vscode.Position(line, 999)),
+        msg,
+        vscode.DiagnosticSeverity.Error
+    )])
 }
 
-// ** Error (suppressible): c:/Users/common/pkg/cbdma_pkg.sv(214): (vlog-13233) Design unit "cbdma_pkg_sv_unit" already exists and will be overwritten. Overwriting SystemVerilog $unit with different source files.
+//----------------------------------------------------------------------------
+// add error wave to text
+function removeErrorOnActiveTextEditor () {
+    collection.delete(vscode.window.activeTextEditor.document.uri)
+}
+
+//----------------------------------------------------------------------------
+module.exports = {
+    updateDiagnostic
+}
