8c1a28a17da7e09e811e608f65bb0a4141d993fc
Nik Henri <nhenri@orthogone.com>
2021-10-22 15:49:58 -0400

    Fix bad regexp

--- src/terminal.js
+++ src/terminal.js
@@ -7,8 +7,8 @@ const vscode = require('vscode')
 // We want to clickable link for ModelSim Time print, example:
 //   #    Time: 0 fs  Iteration: 0  Instance: /tcp_dma_tb/DUT/qmngr_top/local_pointers_if File: C:/src/queue_manager/src/qmngr_top.sv Line: 451
 function provideTerminalLinks(context) {
-    // console.log(`Terminal line: ${context.line}`)
-	let matchArray = Array.from(context.line.matchAll(/([^ ]+) Line: (\\d+)/g))
+    console.log(`Terminal line: ${context.line}`)
+	let matchArray = Array.from(context.line.matchAll(/([^ ]+) Line: (\d+)/g))
 	if (matchArray.length) {
 		return [{startIndex: matchArray[0].index,
 			     length: matchArray[0][0].length,
@@ -20,7 +20,7 @@ function provideTerminalLinks(context) {
 // What to we do when the link is clicked
 // show document at the right line
 function handleTerminalLink(link) {
-    // console.log(`link: ${JSON.stringify(link)}`)
+    //console.log(`link: ${JSON.stringify(link)}`)
 	let pos = new vscode.Position(parseInt(link.line) - 1, 0)
     vscode.workspace.openTextDocument(link.uri)
 	.then((doc) => vscode.window.showTextDocument(doc, {selection: new vscode.Range(pos, pos)}))
